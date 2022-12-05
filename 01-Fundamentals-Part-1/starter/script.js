a8f00413d8b156b6b41e2384823df8081eed0cb8
Amit <amit.samant@gmail.com>
2022-12-05 10:49:11 +0530

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+// let js = "amazing";
+// if (js === "amazing") alert("javascript is fun");
+console.log(40 + 8);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matila";
+console.log(firstName);
+
+// let 3 years = 3
+//we cannot write variable names starting with number
+//do not start with upper case letter - just convention - used for class names
+//all caps letters reserved for constants
+// let name = "Jonas"
+
+let PI = 3.14;
+
+let myFirstJob = "Programmerr";
+console.log(myFirstJob);
+
+//javascript - every value is object or primitive
+//Number - floating point numbers always 23 is 23.0
+//String - sequence of chars -
+//Boolean - true or false
+//Undefined - value taken by variable that is not yet deinfed let variableName;
+//Null
+//Symbol
+//BigInt - larger integers that Number cannot hold
+
+// Javascript is dynamically types
+//Value has a type not the variable
